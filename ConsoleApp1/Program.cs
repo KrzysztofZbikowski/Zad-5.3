860ccae305ff34fb321344b3150a1e150c29c632
Zbiku <Zbiku@MSI>
2023-11-24 22:43:57 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿class zad
+{
+    static void Przesun(ref double x, ref double y, double[] wek)
+    {
+        x = x + wek[0];
+        y = y + wek[1];
+    }
+    static void Main(string[] args)
+    {
+        double[] wek = { 3, 2 };
+        double x, y;
+        Console.WriteLine("Podaj pierwszą współrzędną wektora: ");
+        x = double.Parse(Console.ReadLine());
+        Console.WriteLine("Podaj drugą współrzędną wektora: ");
+        y = double.Parse(Console.ReadLine());
+        Przesun(ref x, ref y, wek);
+        Console.WriteLine("Współrzędne twojego przesuniętego punktu: ({0},{1})", x, y);
+    }
+}
